f8abc9803c1c0125e673e872f5a17cd48de2369f
Benjamin Lim <mail@limbenjamin.com>
2017-11-04 11:13:08 +0800

    Old regex was not working. Changed regex.

--- wp-plugins.py
+++ wp-plugins.py
@@ -7,7 +7,7 @@ BASEURL = "https://wordpress.org/plugins/browse/popular/"
 DESTFILE = "plugins.txt"
 TAGS = ["admin", "administration", "ads", "adsense", "advertising", "affiliate", "AJAX", "amazon", "analytics", "api", "audio", "authentication", "author", "automatic", "bbPress", "blog", "buddypress", "button", "calendar", "captcha", "categories", "category", "Chat", "cms", "code", "comment", "comments", "contact", "contact", "form", "content", "CSS", "custom", "custom", "post", "type", "dashboard", "database", "Digg", "e-commerce", "easy", "ecommerce", "edit", "editor", "email", "embed", "event", "events", "Facebook", "feed", "filter", "flash", "flickr", "form", "Formatting", "forms", "free", "gallery", "google", "google", "analytics", "google", "maps", "google", "plus", "html", "html5", "image", "images", "import", "integration", "iphone", "javascript", "jquery", "language", "lightbox", "Like", "link", "linkedin", "links", "list", "login", "mail", "map", "maps", "marketing", "media", "menu", "meta", "mobile", "multisite", "music", "navigation", "network", "News", "newsletter", "notification", "page", "pages", "password", "payment", "paypal", "performance", "photo", "photos", "php", "picture", "pictures", "pinterest", "plugin", "plugins", "popup", "Post", "posts", "profile", "random", "redirect", "registration", "responsive", "rss", "search", "security", "seo", "Share", "sharing", "shop", "shortcode", "shortcodes", "sidebar", "simple", "slider", "slideshow", "social", "social", "media", "spam", "statistics", "stats", "store", "tag", "tags", "Taxonomy", "template", "text", "theme", "themes", "thumbnail", "thumbnails", "TinyMCE", "title", "tracking", "tweet", "twitter", "upload", "url", "user", "users", "video", "vimeo", "widget", "widgets", "woocommerce", "wordpress", "wpmu", "xml", "yahoo", "youtube"]
 TAGURL = "https://wordpress.org/plugins/tags/"
-regex = re.compile(r'name column-name.+?href="(.*?)">(.*?)</a>.+?column-installs">(.*?)</div>', flags=re.MULTILINE)
+regex = re.compile(r'<h2 class="entry-title"><a href="(.*?)" rel.*?<i class="dashicons dashicons-chart-area"></i>(.*?)active installations.*?<i class="dashicons dashicons-calendar"></i>(.*?)</span>', flags=re.MULTILINE)
 
 
 def create_list(wp_url, file):
@@ -25,9 +25,10 @@ def create_list(wp_url, file):
 		if matches:
 			print("Found plugins..." + str(len(matches)))
 			for match in matches:
-				url, name, installs = match
+				url, installs, lastUpd = match
 				installs = installs.replace('\\t',"").replace('\\n',"")
-				file.write(name + "||" + url + "||" + installs + "\n")
+				lastUpd = lastUpd.replace('\\t',"").replace('\\n',"")
+				file.write(url + "||" + installs + "||" + lastUpd + "\n")
 				file.flush()
 		if not "Next &raquo;" in content:
 			has_next = False
